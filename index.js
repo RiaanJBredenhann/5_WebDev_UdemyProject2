1a0565034b219e3321285f1c46f1ff043b343027
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-11-17 10:25:48 +0200

    finalized mongoDB connection

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
+
+// connecting to database
+mongoose.connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB is Connected"))
+.catch((err) => console.log(err));
+
 
 app.use(express.json());
 
