07b60f0b9c5729677b3ab368129fdb267d6bb2db
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-05-10 10:43:49 +0200

    update

---
+++ client/src/components/Register.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+
+const Register = (props) => {
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    const {name, email, password} = data
+
+    const handleChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post('/auth/register', {name, email, password}, {
+                headers: {
+                    "content-Type": "application/json", 
+                }
+            });
+            props.history.push('/login')
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <div className='form mt-5'>
+            <h4 className='text-mutated text-center mb-5'>Create an Account</h4>
+
+            <div className='card p-5 shadow'>
+                <form>
+                    <div className='form-group'>
+                        <label htmlFor='name'>Name</label>
+                        <input className='form-control' type='name' name='name' value={name} onChange={handleChange}></input>
+                    </div>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input className='form-control' type='email' name='email' value={email} onChange={handleChange}></input>
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <input className='form-control' type='password' name='password' value={password} onChange={handleChange}></input>
+                    </div>
+
+                    <div className='text-center'>
+                        <button className='btn btn-primary' onClick={handleSubmit}>Register</button>
+                    </div>
+                    <p className='mt-3 text-center'>
+                        Already as user? <Link to='/login'>Login</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    )
+}
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
+const cors = require('cors')
 
+// COnnect DB
 mongoose.connect(process.env.MONGO_UR, {
     useCreateIndex: true,
     useNewUrlParser: true,
@@ -12,7 +14,8 @@ mongoose.connect(process.env.MONGO_UR, {
 .then(() => console.log("mongoDB is connected"))
 .catch((err) => console.log(err));
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use("/auth", require('./routes/user'));
 
 const PORT = process.env.PORT || 5000
